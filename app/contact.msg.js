860efd4cd22aed9fe5974e11c5f558e24eb3f448
mark-kang <mark.hs.kang@gmail.com>
2016-05-06 13:28:40 +0900

    moving to cloud9

---
+++ app/contact.msg.js
@@ -0,0 +1,17 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var ContactMsg;
+    return {
+        setters:[],
+        execute: function() {
+            ContactMsg = (function () {
+                function ContactMsg() {
+                }
+                return ContactMsg;
+            }());
+            exports_1("ContactMsg", ContactMsg);
+        }
+    }
+});
+//# sourceMappingURL=contact.msg.js.map
